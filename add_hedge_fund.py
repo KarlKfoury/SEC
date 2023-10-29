162a5d29a06d5b453f56874fdc1dc9e9560d817f
Karl Kfoury <76066593+KarlKfoury@users.noreply.github.com>
2023-10-29 08:37:07 +0200

    Add files via upload

--- add_hedge_fund.py
+++ add_hedge_fund.py
@@ -7,9 +7,9 @@ import sqlite3
 import datetime
 import requests
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from Definitions import doGet
 import sys
-
 
 def scrape_13f(date,url):
 #     date = '2020-08-17'
@@ -18,36 +18,30 @@ def scrape_13f(date,url):
     res = doGet(url)
     soup = BeautifulSoup(res.text, 'lxml')
     rows = soup.find_all('tr')[11:]
-    positions = []
+    positions = []          
+    for row in rows:
+        dic = {}
+        position = row.find_all('td')
+        dic["NAME_OF_ISSUER"] = position[0].text
+        dic["TITLE_OF_CLASS"] = position[1].text
+        dic["CUSIP"] = position[2].text
+        dic["VALUE"] = int(position[3].text.replace(',', ''))*1000
+        dic["SHARES"] = int(position[4].text.replace(',', ''))
+        dic["DATE"] = date.strip(".html")
+        positions.append(dic) 
 
     if len(rows) != 0:
         holdings_df = pd.DataFrame(positions)
-        holdings_df_options = holdings_df[holdings_df['OPTIONS'] != '\xa0']
-        del holdings_df['OPTIONS']
         cusip_nums = holdings_df['CUSIP'].to_list()
-        ticker_dic = {c:"" for c in cusip_nums}
-        for c in list(ticker_dic.keys()):
+        ticker_column = []
+        for c in cusip_nums:
             url = "https://www.openfigi.com/search#!?simpleSearchString="+c
-            driver = webdriver.Chrome(executable_path='C:\Program Files\Google\Chrome\Application')
+            driver = webdriver.Chrome(ChromeDriverManager(version='118').install())
             driver.get(url)
             ticker_elem = driver.find_element_by_css_selector('body > div.outer-wrapper > div.content-wrapper > div > div > div.col-sm-16.col-md-18 > figi-search-grid > div > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > span')
-            ticker = ticker_elem.text
+            ticker_column.append(ticker_elem.text)        
+        holdings_df["SYMBOL"] = ticker_column
         time.sleep(1)
-
-        positions = []
-        for row in rows:
-            dic = {}
-            position = row.find_all('td')
-            dic["SYMBOL"] = ticker_dic[position[2].text]
-            dic["NAME_OF_ISSUER"] = position[0].text
-            dic["TITLE_OF_CLASS"] = position[1].text
-            dic["CUSIP"] = position[2].text
-            dic["VALUE"] = int(position[3].text.replace(',', ''))*1000
-            dic["SHARES"] = int(position[4].text.replace(',', ''))
-            dic["DATE"] = date.strip(".html")
-            positions.append(dic) 
-
-
         holdings_df = pd.DataFrame(positions)
         
         holdings_df['SHARES'] = holdings_df['SHARES'].fillna(0).astype(int)
@@ -127,46 +121,46 @@ except sqlite3.Error as error:
 for cik in CIKs:
     base_url = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={}&count=100&type={}"
     edgar_resp = doGet(base_url.format(cik,type))
-    try:    
-        edgar_str = edgar_resp.text
+#try:    
+    edgar_str = edgar_resp.text
 
 # Find the document link
-        doc_link = ''
-        soup = BeautifulSoup(edgar_str, 'html.parser')
-        table_tag = soup.find('table', class_='tableFile2')
-        rows = table_tag.find_all('tr')
-    
+    doc_link = ''
+    soup = BeautifulSoup(edgar_str, 'html.parser')
+    table_tag = soup.find('table', class_='tableFile2')
+    rows = table_tag.find_all('tr')
+
 
 #Table of all recent listings:
-        table = soup.find_all('table')
-        filings_df = pd.read_html(str(table))[2].head(10)
-        filings_df['URL'] = ''
-        for index,row in zip(filings_df.index.to_list(),rows[1:]):
-            cells = row.find_all('td')
-            s1 = cells[1].a['href']
-            s2 = s1.rsplit('/', 1)[0]
-            if len(cells) > 3:
-                try:
-                    doc_link = 'https://www.sec.gov'+s2+'/xslForm13F_X01/infotable.xml'
-                    filings_df.loc[index,'URL'] = doc_link
-                    time.sleep(1)
-                except:
-                    continue
+    table = soup.find_all('table')
+    filings_df = pd.read_html(str(table))[2].head(10)
+    filings_df['URL'] = ''
+    for index,row in zip(filings_df.index.to_list(),rows[1:]):
+        cells = row.find_all('td')
+        s1 = cells[1].a['href']
+        s2 = s1.rsplit('/', 1)[0]
+        if len(cells) > 3:
+            try:
+                doc_link = 'https://www.sec.gov'+s2+'/xslForm13F_X01/infotable.xml'
+                filings_df.loc[index,'URL'] = doc_link
+                time.sleep(1)
+            except:
+                continue
 # filings_df = filings_df.groupby('Filings').head(2).reset_index(drop=True)
 # filings_df['URL'] = filings_df['URL'].apply(lambda x: '<a href="{0}" target="_blank">{0}</a>'.format(x))
-        filings_df = filings_df[~filings_df.Description.str.contains("Amend")]
-        filings_df['isAmmended'] = False
-        filings_df = filings_df.head(n)
-        filings_df = filings_df.reset_index(drop=True)
-        ammended_filings_df = filings_df[filings_df.Description.str.contains("Amend")]
-        ammended_filings_df['isAmmended'] = False
-        ammended_filings_df = ammended_filings_df.head(n)
-        ammended_filings_df = ammended_filings_df.reset_index(drop=True)
-        for date, url in zip(filings_df['Filing Date'], filings_df.URL):
-            scrape_13f(date,url)
-        for date, url in zip(ammended_filings_df['Filing Date'], ammended_filings_df.URL):
-            scrape_13f(date,url)
-    except AttributeError as error:
-        print(error)
-        print(cik)
+    filings_df = filings_df[~filings_df.Description.str.contains("Amend")]
+    filings_df['isAmmended'] = False
+    filings_df = filings_df.head(n)
+    filings_df = filings_df.reset_index(drop=True)
+    ammended_filings_df = filings_df[filings_df.Description.str.contains("Amend")]
+    ammended_filings_df['isAmmended'] = False
+    ammended_filings_df = ammended_filings_df.head(n)
+    ammended_filings_df = ammended_filings_df.reset_index(drop=True)
+    for date, url in zip(filings_df['Filing Date'], filings_df.URL):
+        scrape_13f(date,url)
+    for date, url in zip(ammended_filings_df['Filing Date'], ammended_filings_df.URL):
+        scrape_13f(date,url)
+    # except AttributeError as error:
+    #     print(error)
+    #     print(cik)
 
